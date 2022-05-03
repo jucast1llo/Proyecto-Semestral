c1bfce5decd8e46345736652feb2954c04e63823
jucast1llo <101342035+jucast1llo@users.noreply.github.com>
2022-05-02 20:19:50 -0400

    REPASO Y ETIQUETADO

--- js/sesion.js
+++ js/sesion.js
@@ -1,27 +1,27 @@
-const nombre = document.getElementById("name")
-const email = document.getElementById("email")
-const pass = document.getElementById("password")
-const form = document.getElementById("form")
-const parrafo = document.getElementById("warnings")
+const nombre = document.getElementById("name") // CONSTANTE  ID NAME
+const email = document.getElementById("email")  // CONSTANTE  ID MAIL
+const pass = document.getElementById("password")  // CONSTANTE  ID PASSWORD
+const form = document.getElementById("form")  // CONSTANTE  ID FORMULARIO
+const parrafo = document.getElementById("warnings")   // CONSTANTE PARRAFO ID ADERTENCIAS
 
-form.addEventListener("submit", e=>{
+form.addEventListener("submit", e=>{  //FUNCION PARA ESCUCHAR EL EVENTO SUBMIT, FUNCION FLECHA 
     e.preventDefault()
-    let warnings = ""
+    let warnings = "" 
     let entrar = false
-    let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/
+    let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/ /* EXPRECIONES REGULARES, PARA QUE COINCIDA LOS CARACTERES DE LA CADENA @ - .COM */
     parrafo.innerHTML = ""
-    if(nombre.value.length <6){
-        warnings += ` - El nombre no es valido, debe ser mayor o igual a 6 caracteres. <br>`
+    if(nombre.value.length <6){  //SI  NOMBRE ES  MAYOR A 6
+        warnings += ` - El nombre no es valido, debe ser mayor o igual a 6 caracteres. <br>`  // CONCATENACION WARNING  Y MSJ
         entrar = true
     }
-    if(pass.value.length < 8){
-        warnings += ` - La contraseña no es valida, debe ser mayor o igual a 8 caracteres. <br>`
+    if(pass.value.length < 8){    // SI  CONTRASEÑA ES  MAYOR A 8
+        warnings += ` - La contraseña no es valida, debe ser mayor o igual a 8 caracteres. <br>`  // CONCATENACION WARNING  Y MSJ
         entrar = true
     }
 
-    if(entrar){
+    if(entrar){ // SI ENTRAR  ES VERDADERO ARROJA LA ADVERTENCIA
         parrafo.innerHTML = warnings
     }else{
-        parrafo.innerHTML = "Enviado"
+        parrafo.innerHTML = "Enviado" // SI ENTRAR  ES FALSO ARROJA LA ADVERTENCIA
     }
 })
