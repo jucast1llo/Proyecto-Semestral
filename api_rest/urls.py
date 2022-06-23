0e41f1ea2ef1b9ab268b3b044c2a5d5aa94ed569
Juan Castillo <101342035+jucast1llo@users.noreply.github.com>
2022-06-22 22:48:10 -0400

    rectificando

--- api_rest/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from api_rest.views import lista_peliculas,agregar_pelicula,Elimpelicula
-from api_rest.viewsLogin import login
-
-urlpatterns = [
-    path('lista_peliculas/',lista_peliculas,name="lista_peliculas"),
-    path('agregar_pelicula/',agregar_pelicula,name="agregar_pelicula"),
-    path('Elimpelicula/<codigo>',Elimpelicula,name="Elimpelicula"),
-]
--- app/serializers.py
+++
@@ -1,8 +0,0 @@
-from dataclasses import field, fields
-from .models import PeliculaProducto
-from rest_framework import serializers
-
-class ProductoSerializers(serializers.ModelSerializer):
-    class Meta:
-        model = PeliculaProducto
-        fields = '__all__'
