dbb419da3d1108f9e08cf0dea15d1e80eeba5cef
Juan Castillo <101342035+jucast1llo@users.noreply.github.com>
2022-06-24 13:29:29 -0400

    comentando

--- app/views.py
+++ app/views.py
@@ -10,22 +10,15 @@ from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required, permission_required
 from django.contrib import messages
 
+### VISTAS PELISLANDIA ###
 
-
-# INICIO #
+# HOME #
 def home(request):
     productos = PeliculaProducto.objects.all()
     data = { 
         'productos': productos
     }
     return render(request, 'app/home.html', data)
-
-def Contactopeli(request):
-    productos = Contacto.objects.all()
-    data = { 
-        'productos': productos
-    }
-    return render(request, 'app/listacontacto.html', data)
 
 # CONTACTO #
 def contacto(request):
@@ -50,94 +43,13 @@ def galeria(request):
     }
     return render(request, 'app/galeria.html', data)
 
-# GALERIA#
+# WALLPAPER #
 def wallpaper(request):
     productos = PeliculaProducto.objects.all()
     data = { 
         'productos': productos
     }
     return render(request, 'app/wallpaper.html', data)
-
-# AGREGAR #
-@permission_required('app.add_producto')
-def agregar_producto(request):
-    data = {
-        'form': ProductoForm()
-    }
-    if request.method == 'POST':
-        formulario = ProductoForm(data=request.POST, files=request.FILES)
-        if formulario.is_valid():
-            formulario.save()
-            messages.success(request, "Pelicula Agregada")
-        else:
-            data["form"] = formulario
-    return render(request, 'app/producto/agregar.html', data)
-
-# LISTAR PELICULAS #
-@permission_required('app.view_producto')
-def listar_productos(request):
-    productos = PeliculaProducto.objects.all()
-    page = request.GET.get('page',1)
-    try:
-        paginator = Paginator(productos, 3)
-        productos = paginator.page(page)
-    except:
-        raise Http404
-
-    data = { 
-        'entity': productos,
-        'paginator': paginator
-    }
-    return render(request, 'app/producto/listar.html', data)
-
-# LISTAR PELICULAS #
-@permission_required('app.view_producto')
-def listacontacto(request):
-    productos = Contacto.objects.all()
-    page = request.GET.get('page',1)
-    try:
-        paginator = Paginator(productos, 3)
-        productos = paginator.page(page)
-    except:
-        raise Http404
-
-    data = { 
-        'entity': productos,
-        'paginator': paginator
-    }
-    return render(request, 'app/producto/listacontacto.html', data)
-
-# MODIFICAR #
-@permission_required('app.change_producto')
-def modificar_producto(request, id):
-    producto = get_object_or_404(PeliculaProducto, id=id)
-    data = { 
-        'form': ProductoForm(instance=producto)
-    }
-    if request.method == 'POST':
-        formulario = ProductoForm(data=request.POST, instance=producto, files=request.FILES)
-        if formulario.is_valid():
-            formulario.save()
-            messages.success(request, "Modificacion Exitosa")
-            return redirect(to="listar_productos")
-        data["form"] = formulario   
-    return render(request, 'app/producto/modificar.html', data)
-
-# ELIMINAR PELICULA #
-@permission_required('app.delete_producto')
-def eliminar_producto(request, id):
-    producto = get_object_or_404(PeliculaProducto, id=id)
-    producto.delete()
-    messages.success(request, "Eliminacion Exitosa")
-    return redirect(to="listar_productos")
-
-# ELIMINAR COMENTARIO #
-@permission_required('app.delete_producto')
-def eliminar_comentario(request, id):
-    producto = get_object_or_404(Contacto, id=id)
-    producto.delete()
-    messages.success(request, "Mensaje Eliminado")
-    return redirect(to="listacontacto")
 
 # REGISTRO #
 def registro(request):
@@ -157,7 +69,51 @@ def registro(request):
     return render(request, 'registration/registro.html', data)
 
 
-# LISTAR USUARIOS #
+### SUBMENU VISTAS PELISLANDIA ###
+
+# LISTA DE CONTACTOS #
+def Contactopeli(request):
+    productos = Contacto.objects.all()
+    data = { 
+        'productos': productos
+    }
+    return render(request, 'app/listacontacto.html', data)
+
+# PAGINAS LISTAR PELICULAS #
+@permission_required('app.view_producto')
+def listar_productos(request):
+    productos = PeliculaProducto.objects.all()
+    page = request.GET.get('page',1)
+    try:
+        paginator = Paginator(productos, 3)
+        productos = paginator.page(page)
+    except:
+        raise Http404
+
+    data = { 
+        'entity': productos,
+        'paginator': paginator
+    }
+    return render(request, 'app/producto/listar.html', data)
+
+#  PAGINAS LISTAR CONTACTO #
+@permission_required('app.view_producto')
+def listacontacto(request):
+    productos = Contacto.objects.all()
+    page = request.GET.get('page',1)
+    try:
+        paginator = Paginator(productos, 3)
+        productos = paginator.page(page)
+    except:
+        raise Http404
+
+    data = { 
+        'entity': productos,
+        'paginator': paginator
+    }
+    return render(request, 'app/producto/listacontacto.html', data)
+
+# PAGINAS LISTAR USUARIOS #
 @permission_required('app.view_producto')
 def listausuario(request):
     productos = User.objects.all()
@@ -173,3 +129,50 @@ def listausuario(request):
         'paginator': paginator
     }
     return render(request, 'app/producto/listausuario.html', data)
+
+# AGREGAR PELICULA #
+@permission_required('app.add_producto')
+def agregar_producto(request):
+    data = {
+        'form': ProductoForm()
+    }
+    if request.method == 'POST':
+        formulario = ProductoForm(data=request.POST, files=request.FILES)
+        if formulario.is_valid():
+            formulario.save()
+            messages.success(request, "Pelicula Agregada")
+        else:
+            data["form"] = formulario
+    return render(request, 'app/producto/agregar.html', data)
+
+# MODIFICAR PELICULAS #
+@permission_required('app.change_producto')
+def modificar_producto(request, id):
+    producto = get_object_or_404(PeliculaProducto, id=id)
+    data = { 
+        'form': ProductoForm(instance=producto)
+    }
+    if request.method == 'POST':
+        formulario = ProductoForm(data=request.POST, instance=producto, files=request.FILES)
+        if formulario.is_valid():
+            formulario.save()
+            messages.success(request, "Modificacion Exitosa")
+            return redirect(to="listar_productos")
+        data["form"] = formulario   
+    return render(request, 'app/producto/modificar.html', data)
+
+# ELIMINAR PELICULA DE LA LISTA #
+@permission_required('app.delete_producto')
+def eliminar_producto(request, id):
+    producto = get_object_or_404(PeliculaProducto, id=id)
+    producto.delete()
+    messages.success(request, "Eliminacion Exitosa")
+    return redirect(to="listar_productos")
+
+# ELIMINAR COMENTARIO DE LA LISTA CONTACTO #
+@permission_required('app.delete_producto')
+def eliminar_comentario(request, id):
+    producto = get_object_or_404(Contacto, id=id)
+    producto.delete()
+    messages.success(request, "Mensaje Eliminado")
+    return redirect(to="listacontacto")
