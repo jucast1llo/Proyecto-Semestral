ccf76dff7727d0e9a2f203ed00ba1ad0438f42ec
jucast1llo <101342035+jucast1llo@users.noreply.github.com>
2022-07-10 17:59:26 -0400

    end

--- Pelislandia/settings.py
+++ Pelislandia/settings.py
@@ -90,7 +90,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.oracle',
         'NAME': '127.0.0.1:1521/xe',
-        'USER': 'prueba',
+        'USER': 'admin',
         'PASSWORD': '1234',
         'TEST': {
             'USER': 'default_test',
